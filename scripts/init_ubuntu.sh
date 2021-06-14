83e68dc936d2bc3f19d284627886e5a0ed4f960e
Petri Sadinmäki <60505390+qubilea-sadinmaki@users.noreply.github.com>
2021-06-14 13:19:16 +0300

    Create init_ubuntu.sh

---
+++ scripts/init_ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# this script is only tested on Ubuntu 18.04 (LTS) x64
+
+# install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+systemctl enable docker
+systemctl start docker
+# usermod -aG docker ubuntu
+
+# run jenkins-docker-network
+mkdir -p jenkins_home
+chown -R 1000:1000 jenkins_home/
+mkdir -p jenkins
+wget https://raw.githubusercontent.com/qubilea-sadinmaki/jenkins-docker-network/main/jenkins/Dockerfile
+mv Dockerfile jenkins
+wget https://raw.githubusercontent.com/qubilea-sadinmaki/jenkins-docker-network/main/docker-compose.yml
+docker-compose up -d
+
+# show endpoint
+echo 'Jenkins installed'
+echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
+echo 'Jenkins initial password below'
+echo | cat jenkins_home/secrets/initialAdminPassword
