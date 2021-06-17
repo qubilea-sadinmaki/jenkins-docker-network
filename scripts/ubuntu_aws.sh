e99ccbe0d1b1e6b5434dbce692a09d60d60a1eda
Petri Sadinmäki <petri.sadinmaki@qubilea.fi>
2021-06-17 12:24:02 +0300

    Update ubuntu_aws.sh
    remove sudo's

--- scripts/ubuntu_aws.sh
+++ scripts/ubuntu_aws.sh
@@ -3,16 +3,17 @@
 # this script is only tested on Ubuntu 18.04 (LTS) x64
 # login: ssh -i /pathto/keypair.pem ubuntu@ec2-xx-xxx-xxx-xxx.eu-central-1.compute.amazonaws.com
 # install docker & docker-compose
-sudo apt-get update
+apt-get update
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sh get-docker.sh
+# dockerd-rootless-setuptool.sh install
 # sudo usermod -aG docker ubuntu
-sudo apt install -y docker-compose
+apt install -y docker-compose
 
 # run jenkins-docker-network
-sudo mkdir -p jenkins_home
-sudo chown -R 1000:1000 jenkins_home/
-sudo mkdir -p jenkins
+mkdir -p jenkins_home
+chown -R 1000:1000 jenkins_home/
+mkdir -p jenkins
 wget https://raw.githubusercontent.com/qubilea-sadinmaki/jenkins-docker-network/main/jenkins/Dockerfile
 mv Dockerfile jenkins
 wget https://raw.githubusercontent.com/qubilea-sadinmaki/jenkins-docker-network/main/docker-compose.yml
